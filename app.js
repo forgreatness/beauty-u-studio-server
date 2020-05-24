b451aa89d997e227e6cdbd8588f7a8d26611035b
Michael Levan <mlevan1992@gmail.com>
2020-05-23 20:15:00 -0400

    date express api

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    var today = new Date()
+    res.send({
+        today,
+        "Name": "CloudDev"
+    })
+    
+})
+
+app.listen(port)
