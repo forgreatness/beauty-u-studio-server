f19174304624b97e632a845f1647b49be76055e5
Michael Levan <mlevan1992@outlook.com>
2020-05-24 10:20:44 -0400

    slash out

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
     var today = new Date()
     res.send(today)
     
