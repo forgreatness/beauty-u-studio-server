8214163e796508d7f9d793a3a1706497685a46fa
Michael Levan <mlevan1992@outlook.com>
2020-05-24 10:31:29 -0400

    Create server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 80
+const server = require('http').createServer().listen(80)
 
 app.get('', (req, res) => {
     var today = new Date()
