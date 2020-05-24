f0aec6e63853a3082d22b2de5a2a72fcb1b46274
Michael Levan <mlevan1992@outlook.com>
2020-05-24 10:38:10 -0400

    port change

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
-const server = require('http').createServer().listen(80)
+const port = process.env.PORT || 80
 
 app.get('', (req, res) => {
     var today = new Date()
