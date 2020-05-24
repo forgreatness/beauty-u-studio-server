d39ea2a3ada6f2410ce69bd0e506e79a740e1697
Michael Levan <mlevan1992@outlook.com>
2020-05-24 10:54:24 -0400

    time

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 80
 app.get('', (req, res) => {
     var today = new Date()
 
-    res.write(today)
-    res.end()
+    time = today.getTime
+    res.send(time)
     
 })
 
