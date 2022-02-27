46a91b6258d4d3f0aa57116e81501bb7216fab86
forgreatness <danh1nguyen23@live.com>
2022-02-26 22:19:39 -0800

    modify get appointments to include stylist own scheduled appointments

--- datasources/BeautyUStudioDB.js
+++ datasources/BeautyUStudioDB.js
@@ -397,6 +397,10 @@ module.exports = class BeautyUStudioDB extends DataSource {
                     query.push({
                         stylist: ObjectID.createFromHexString(filter.stylist.toString())
                     });
+
+                    query.push({
+                        client: ObjectID.createFromHexString(filter.stylist.toString())
+                    });
                 }
 
                 if (filter.client != null) {
